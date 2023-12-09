9557d99f7ed6da04b8ec274ff611894869192530
Sam Uherek <samuherekbiz@gmail.com>
2023-12-09 10:24:46 +0100

    really spagheti day3 part2 solution

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
 
+fn is_digit(char: &char) -> bool {
+    char::is_digit(char.clone(), 10)
+}
+
 fn is_symbol(char: char) -> bool {
     !char::is_digit(char, 10) && char != '.'
 }
@@ -69,3 +73,129 @@ pub fn part1_solve(data: &String) -> Result<u32> {
 
     return Ok(sum);
 }
+
+
+fn parse_left(val: Option<&str>) -> Option<String> {
+    return val.and_then(|val| {
+        let reversed = val
+            .chars()
+            .into_iter()
+            .rev()
+            .take_while(is_digit)
+            .collect::<Vec<char>>();
+
+        return Some(reversed.iter().rev().collect());
+    });
+}
+
+fn parse_right(val: Option<&str>) -> Option<String> {
+    return val.and_then(|val| {
+        return Some(val.chars().into_iter().take_while(is_digit).collect());
+    })
+}
+
+fn parse_row(row: &str, x: usize) -> Vec<u32>{
+    let mut valid_nums = Vec::new();
+    let mut temp_num = String::new();
+
+    let left_x = x.saturating_sub(1);
+    let left_val = row.chars().nth(left_x).unwrap_or(' ');
+    let center_val = row.chars().nth(x).unwrap_or(' ');
+    let right_x = if x >= row.len() {
+        row.len().saturating_sub(1)
+    } else {
+        x + 1
+    };
+    let right_val = row.chars().nth(right_x).unwrap_or(' ');
+
+
+    if is_digit(&left_val) {
+        if let Some(num) = parse_left(row.get(..=left_x)) {
+            temp_num.push_str(num.as_str());
+        }
+    }
+
+
+    if is_digit(&center_val) {
+        temp_num.push(center_val);
+    } else if !temp_num.is_empty() {
+        if let Ok(num) = temp_num.parse::<u32>() {
+            valid_nums.push(num);
+        }
+
+        temp_num.clear();
+    }
+
+    if is_digit(&right_val) { 
+        if let Some(num) = parse_right(row.get(right_x..)) {
+            temp_num.push_str(num.as_str());
+        }
+    } else if !temp_num.is_empty() {
+        if let Ok(num) = temp_num.parse::<u32>() {
+            valid_nums.push(num);
+        }
+
+        temp_num.clear();
+    }
+
+
+    if !temp_num.is_empty() {
+        if let Ok(num) = temp_num.parse::<u32>() {
+            valid_nums.push(num);
+        }
+
+        temp_num.clear();
+    }
+
+    return valid_nums;
+}
+
+fn adjecent_nums(rows: &Vec<&str>, (y, x): (usize, usize)) -> Vec<u32> {
+    let top_idx = y.saturating_sub(1);
+    let bottom_idx = if y >= rows.len() {
+        rows.len().saturating_sub(1)
+    } else {
+        y + 1
+    };
+
+    let top = rows.get(top_idx).and_then(|row| {
+        return Some(parse_row(row, x));
+    }).unwrap_or(Vec::new());
+
+    let center = rows.get(y).and_then(|row| {
+        return Some(parse_row(row, x));
+    }).unwrap_or(Vec::new());
+
+    let bottom = rows.get(bottom_idx).and_then(|row| {
+        return Some(parse_row(row, x));
+    }).unwrap_or(Vec::new());
+
+    let mut result = Vec::new();
+    result.extend(top);
+    result.extend(center);
+    result.extend(bottom);
+
+    return result;
+}
+
+pub fn part2_solve(data: &String) -> Result<u32> {
+    let rows = data.lines().collect::<Vec<&str>>();
+    let mut sum = 0;
+
+    for (row_idx, line) in rows.iter().enumerate() {
+        for (char_idx, val) in line.chars().enumerate() {
+            match val {
+                '*' => {
+                    let nums = adjecent_nums(&rows, (row_idx, char_idx));
+                    if nums.len() > 1 { 
+                        sum += nums.iter().fold(1, |acc, num| acc * num);
+                    }
+                },
+                _ => {}
+            }
+        }
+    }
+
+    return Ok(sum);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 
 fn main() {
     let data = fs::read_to_string(PathBuf::from("data/day3")).unwrap();
-    let result = day3::part1_solve(&data).unwrap();
+    let result = day3::part2_solve(&data).unwrap();
 
     //println!("this is the input\n{data}");
     println!("result is:\n{result}");
