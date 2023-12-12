03d6ea0de09ae0dc61dd6fb2240c56707c388d25
Sam Uherek <samuherekbiz@gmail.com>
2023-12-12 20:10:53 +0100

    ugly, but works day4 part 2

--- 2023/src/day4.rs
+++ 2023/src/day4.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 pub fn part1_solve(data: &String) -> Result<u32> {
@@ -33,4 +35,42 @@ pub fn part1_solve(data: &String) -> Result<u32> {
     }).sum::<u32>();
 
     return Ok(sum);
+}
+
+pub fn part2_solve(data: &String) -> Result<usize> {
+    let d = data.lines().enumerate().fold(HashMap::new(), |mut map, (game_idx, line)| {
+        let (_, game) = line.split_once(":").unwrap_or(("", ""));
+        let _ = game.split_once("|").and_then(|(target, guess)| {
+            let target_nums: Vec<u32> = target.trim()
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap_or(0))
+                .filter(|x| x > &0)
+                .collect();
+            let guess_nums: Vec<u32> = guess.trim()
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap_or(0))
+                .filter(|x| x > &0)
+                .collect();
+            
+            let curr_cards_count: usize = map.entry(game_idx + 1).or_insert(1).clone();
+            let winner_count = target_nums.iter()
+                .filter(|n| guess_nums.iter().find(|x| x == n).is_some())
+                .count();
+
+            for c in 1..=winner_count {
+                *map.entry(game_idx + 1 + c).or_insert(1) += curr_cards_count;
+            }
+
+            // get the count num of the winners for an index
+            // Add one card for from index + 1 until ..winners
+            // Next itteration find winners + double it for index card count
+            //
+            return Some(());
+        });
+
+        return map;
+    });
+
+
+    return Ok(d.values().sum());
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 
 fn main() {
     let data = fs::read_to_string(PathBuf::from("data/day4")).unwrap();
-    let result = day4::part1_solve(&data).unwrap();
+    let result = day4::part2_solve(&data).unwrap();
 
     //println!("this is the input\n{data}");
     println!("result is:\n{result}");
