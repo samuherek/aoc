38ca46a6b6dfbd3e081b20b8c371f5bcd40deab4
Sam Uherek <samuherekbiz@gmail.com>
2024-12-03 19:39:30 +0100

    day 3 part 2

--- 2024/src/aoc_3/mod.rs
+++ 2024/src/aoc_3/mod.rs
@@ -57,9 +57,78 @@ fn part1() {
     println!("result: {result}");
 }
 
+const DONT: &str = "don't()";
+const DO: &str = "do()";
+
 fn part2() {
+    let data = fs::read_to_string("./src/aoc_3/input.txt").unwrap();
+    let mut result = 0;
+    let mut cursor = 0;
+    let mut enabled = true;
+
+    while cursor < data.len() {
+        if data[cursor..].starts_with(DONT) {
+            enabled = false;
+            cursor += DONT.len();
+        } else if data[cursor..].starts_with(DO) {
+            enabled = true;
+            cursor += DO.len();
+        } else if data[cursor..].starts_with("mul(") {
+            cursor += 4;
+
+            let mut left = String::new();
+            while let Some(char) = data[cursor..].chars().next() {
+                if char.is_digit(10) {
+                    left.push(char);
+                    cursor += 1;
+                } else {
+                    break;
+                }
+            }
+            // println!("left {left}");
+
+            if left.len() == 0 || left.len() > 3 {
+                continue;
+            }
+
+            let mut right = String::new();
+            if data[cursor..].chars().next() == Some(',') {
+                cursor += 1;
+                while let Some(char) = data[cursor..].chars().next() {
+                    if char.is_digit(10) {
+                        right.push(char);
+                        cursor += 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                continue;
+            }
+            // println!("right: {right}");
+
+            if right.len() == 0 || right.len() > 3 {
+                continue;
+            }
+
+            if data[cursor..].chars().next() != Some(')') {
+                cursor += 1;
+                continue;
+            }
+
+            if enabled {
+                let mult = left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap();
+                // println!("mult: {left}x{right} = {mult}");
+                result += mult;
+            }
+        } else {
+            cursor += 1;
+        }
+    }
+
+    println!("result: {result}");
 }
 
 pub fn solve() {
-    part1();
+    part2();
 }
