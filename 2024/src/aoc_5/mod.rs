3befa3a3b22d3cdf5be6c815bd06a84ea2c8d0f7
Sam Uherek <samuherekbiz@gmail.com>
2024-12-05 11:30:40 +0100

    day 5 part 2

--- 2024/src/aoc_5/mod.rs
+++ 2024/src/aoc_5/mod.rs
@@ -32,12 +32,6 @@ const TEXT_INPUT: &str = r#"
 97,13,75,29,47
 "#;
 
-const TEST_OUTPUT: &str = r#"
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-"#;
-
 #[derive(Debug)]
 struct Rule(u32, u32);
 
@@ -123,8 +117,84 @@ fn part1(data: String) -> u32 {
     result
 }
 
-fn part2(data: String) -> usize {
-    0
+fn part2(data: String) -> u32 {
+    let mut rules = vec![];
+    let mut updates = vec![];
+    let mut result = 0;
+
+    for line in data.lines() {
+        if line.contains("|") {
+            let (left, right) = line.split_once("|").unwrap();
+            rules.push(Rule::new(left, right));
+        } else if line.contains(",") {
+            let pages = line
+                .split(',')
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<_>>();
+            updates.push(pages);
+        }
+    }
+
+    for page_order in updates.iter_mut() {
+        let curr_rules: Vec<_> = rules
+            .iter()
+            .filter(|x| page_order.iter().any(|y| x.contains(*y)))
+            .collect();
+
+        // 97|13
+        // 97|61
+        // 97|47
+        // 97|29
+        // 97|53
+        // 97|75
+        //
+        // 75|29
+        // 75|53
+        // 75|47
+        // 75|13
+        //
+        // 29|13
+        //
+
+        // 75,97,47,61,53 becomes 97,75,47,61,53.
+        // 61,13,29 becomes 61,29,13.
+        // 97,13,75,29,47 becomes 97,75,47,29,13.
+        // 97,75,13,29,47
+        // 97,75,29,13,47
+        // 97,75,47,13,29
+        // 97,75,47,29,13
+        let mut is_valid = true;
+        let mut cursor = 0;
+        while cursor < page_order.len() {
+            let page_cursor_rules = curr_rules
+                .iter()
+                .filter(|r| r.before() == page_order[cursor])
+                .collect::<Vec<_>>();
+
+            for i in 0..cursor {
+                if page_cursor_rules.iter().any(|r| r.after() == page_order[i]) {
+                    is_valid = false;
+                    page_order.swap(i, cursor);
+                    cursor = i;
+                    break;
+                }
+            }
+
+            cursor += 1;
+        }
+
+        if !is_valid {
+            // println!("invalid: {page_order:?}");
+            if page_order.len() % 2 == 0 {
+                panic!("Unexpected input");
+            } else {
+                let midd_idx = (page_order.len() - 1) / 2;
+                result += page_order[midd_idx];
+            }
+        }
+    }
+
+    result
 }
 
 pub fn solve() {
@@ -133,6 +203,6 @@ pub fn solve() {
         InputMode::Test => TEXT_INPUT.to_string(),
         InputMode::Source => fs::read_to_string("./src/aoc_5/input.txt").unwrap(),
     };
-    let result = part1(data);
+    let result = part2(data);
     println!("reuslt: {result}");
 }
