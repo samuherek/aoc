bbd74b32c0d7e27cf4676125153810d13816d701
Sam Uherek <samuherekbiz@gmail.com>
2024-12-09 19:59:21 +0100

    day 1 part 2

--- 2015/aoc_1/main.py
+++ 2015/aoc_1/main.py
@@ -11,7 +11,10 @@ expected = 3
 INPUT_4 = "))("
 expected = -1
 
-def calc(input):
+INPUT_5 = "()())"
+expected = 5
+
+def part1(input):
     input = input.read().strip()
     val = 0
     for c in input:
@@ -21,7 +24,22 @@ def calc(input):
             val = val - 1
     return val
 
+def part2(input):
+    val = 0
+    result = 0;
+    for i in range(len(input)):
+        if input[i] == "(":
+            val = val + 1
+        elif input[i] == ")":
+            val = val - 1
+        if val < 0:
+            result = i + 1
+            break;
+
+    return result
+
 with open("input.txt") as file:
-    print(calc(file))
+    input = file.read().strip()
+    print(part2(input))
 
 
